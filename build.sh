451c500b14b3b5e38aaae822648924bf312e2470
monster1237 <113280444+monster1237@users.noreply.github.com>
2024-07-08 18:32:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+termux-info
+pkg update
+pkg upgrade
+pkg install libjpeg-turbo python micro -y
+pip install --upgrade pip wheel setuptools
 pip install --upgrade tgcf
 tgcf --version
+tgcf --help
